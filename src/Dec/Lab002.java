2fa79bf942d6ab0209ae17017f5abcaf0c1f787e
LITERAMS\shubham.sharma <Shubham.sharma@litera.com>
2024-12-27 19:31:26 +0530

    All changes done

---
+++ src/Dec/Lab002.java
@@ -0,0 +1,10 @@
+package Dec;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        System.out.println(10+2);//Add
+        System.out.println(10-2);//Subtract
+        System.out.println(10*2);//Multiply
+        System.out.println(10/2);// Divide
+    }
+}
---
+++ src/Dec/Lab003.java
@@ -0,0 +1,7 @@
+package Dec;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
