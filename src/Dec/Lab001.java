b8f62dac0bd5c7ac64d0f6d9911bac21ad512a12
LITERAMS\shubham.sharma <Shubham.sharma@litera.com>
2024-12-28 17:59:23 +0530

    Changes done

--- src/Dec/Lab001.java
+++ src/Dec/Lab001.java
@@ -9,15 +9,24 @@ public class Lab001 {
      */
 
     public static void main(String[] args) {
-        System.out.println("10*1=10");
-        System.out.println("10*2=20");
-        System.out.println("10*3=30");
-        System.out.println("10*4=40");
-        System.out.println("10*5=50");
-        System.out.println("10*6=60");
-        System.out.println("10*7=70");
-        System.out.println("10*8=80");
-        System.out.println("10*9=90");
-        System.out.println("10*10=100");
+
+       for (int i=1;i<=10;i++){
+
+           System.out.println("10*"+i+ "="+ 10*i);
+       }
+
+
+
+
+//        System.out.println("10*1=10");
+//        System.out.println("10*2=20");
+//        System.out.println("10*3=30");
+//        System.out.println("10*4=40");
+//        System.out.println("10*5=50");
+//        System.out.println("10*6=60");
+//        System.out.println("10*7=70");
+//        System.out.println("10*8=80");
+//        System.out.println("10*9=90");
+//        System.out.println("10*10=100");
     }
 }
