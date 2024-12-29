9db56dd01070bb563f98cbef8eeaa61adc59655b
LITERAMS\shubham.sharma <Shubham.sharma@litera.com>
2024-12-29 22:36:29 +0530

    New changes done

---
+++ src/Dec/Lab004.java
@@ -0,0 +1,25 @@
+package Dec;
+
+public class Lab004 {
+    public static void main(String[] args) {
+//        String name="Shubham";
+//        System.out.printf("Hi, There %s",name);
+        int table=9;
+
+        System.out.printf("%d x %d = %d",table,1,table);
+        System.out.println();
+        System.out.printf("%d x %d = %d",table,2,table*2);
+        System.out.println();
+        System.out.printf("%d x %d = %d",table,3,table*3);
+        System.out.println();
+        System.out.printf("%d x %d = %d",table,4,table*4);
+        System.out.println();
+        System.out.printf("%d x %d = %d",table,5,table*5);
+
+//        float f=3.14f;
+//        System.out.printf("This is value of pi in decimal %f",f);
+
+
+
+    }
+}
