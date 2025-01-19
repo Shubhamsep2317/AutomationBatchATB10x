8299adbf23e1685c6c2626970786d2a213f956eb
LITERAMS\shubham.sharma <Shubham.sharma@litera.com>
2025-01-19 19:47:29 +0530

    Changes 19th Jan2025

---
+++ src/Tasks/task19012025_Functions.java
@@ -0,0 +1,39 @@
+package Tasks;
+
+import java.util.Scanner;
+
+public class task19012025_Functions {
+     static int addition(int a,int b){
+         return (a+b);
+     }
+    static int subtraction(int a,int b){
+        return (a-b);
+    }
+    static int multiplication(int a,int b){
+        return (a*b);
+    }
+    static int modulus(int a,int b){
+        return (a%b);
+    }
+    static int division(int a,int b){
+        return (a/b);
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner sn=new Scanner(System.in);
+        System.out.println("Enter the value of A\n");
+        int A =sn.nextInt();
+        System.out.println("Enter the value of B\n");
+        int B =sn.nextInt();
+
+        int result=modulus(A,B);
+        System.out.println(result);
+
+        int result1=multiplication(A,B);
+        System.out.println(result1);
+
+
+    }
+}
