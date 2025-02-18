5e17d7d3517747d29dd5e8e18cd489d2f31fbcde
LITERAMS\shubham.sharma <Shubham.sharma@litera.com>
2025-02-18 22:25:27 +0530

    New changes done 18th Feb

---
+++ src/Tasks/ExtraTasksFeb/Interview_1.java
@@ -0,0 +1,30 @@
+package Tasks.ExtraTasksFeb;
+
+public class Interview_1 {
+
+    public static void main(String[] args) {
+
+        String input = "vision without action is day dream";
+
+        String[] splitString = input.split(" ");
+
+        for (int i = 0; i < splitString.length; i++) {
+            System.out.println(splitString[i]);
+        }
+        int minLength = splitString[0].length();
+        int maxLength = splitString[0].length();
+
+        for (int i = 0; i < splitString.length; i++) {
+            if (minLength > splitString[i].length()) {
+                minLength = splitString[i].length();
+                System.out.println("The minimum length string in the input is---->>>" + splitString[i]);
+            }
+        }
+        for (int i = 0; i < splitString.length; i++) {
+            if (maxLength < splitString[i].length()) {
+                maxLength = splitString[i].length();
+                System.out.println("The maximum length string in the input is---->>>" + splitString[i]);
+            }
+        }
+    }
+}
