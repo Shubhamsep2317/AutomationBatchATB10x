a1f4ab5fa7c5ad359b3098a847199a326574a8b8
LITERAMS\shubham.sharma <Shubham.sharma@litera.com>
2025-02-22 09:01:10 +0530

    New changes done 18th Feb

--- src/Tasks/ExtraTasksFeb/Interview_1.java
+++ src/Tasks/ExtraTasksFeb/Interview_1.java
@@ -6,7 +6,7 @@ public class Interview_1 {
 // Program to find max and min string words in a given input string
     public static void main(String[] args) {
 
-        String input = "vision without action is day dream";
+        String input = "vision without action a day dream";
 
         String[] splitString = input.split(" ");
 
@@ -20,12 +20,11 @@ public class Interview_1 {
             if (minLength > splitString[i].length()) {
                 minLength = splitString[i].length();
                 System.out.println("The minimum length string in the input is---->>>" + splitString[i]);
-            }
-        }
-        for (int i = 0; i < splitString.length; i++) {
-            if (maxLength < splitString[i].length()) {
-                maxLength = splitString[i].length();
-                System.out.println("The maximum length string in the input is---->>>" + splitString[i]);
+            } else {
+                if (maxLength < splitString[i].length()) {
+                    maxLength = splitString[i].length();
+                    System.out.println("The maximum length string in the input is---->>>" + splitString[i]);
+                }
             }
         }
     }
