bcaa1ef905940a26908b4640edfa0f96a06eb396
Shubham <Shubhamsep23@gmail.com>
2025-04-03 09:08:18 +0530

    3rd April interview Question

---
+++ src/Tasks/April/InterviewQ.java
@@ -0,0 +1,37 @@
+package Tasks.April;
+// WAP to add the digits in a given string "automation 658engineer" --->> o/p--->
+// "automation 19engineer"
+
+public class InterviewQ {
+
+    public static void main(String[] shubham) {
+
+        String designation = "automation 658engineer";
+        String num = "";
+        String characters = "";
+
+
+        for (char ch : designation.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                num += ch;
+            } else {
+                characters += ch;
+            }
+        }
+        System.out.println(num);
+        System.out.println(characters);
+
+        int sum = 0;
+        for (char ch : num.toCharArray()) {
+            sum += Character.getNumericValue(ch);
+        }
+
+        System.out.println(sum);
+
+        characters= characters.replace(" "," "+sum);
+        System.out.println(characters);
+
+    }
+
+
+}
