1e687928e0f59d482f607f32fe8f879c09ea6703
Shubham <Shubhamsep23@gmail.com>
2025-04-24 18:57:18 +0530

    24th April interview Question

---
+++ src/Tasks/April/Interview_Q_06LargestSubstring.java
@@ -0,0 +1,30 @@
+package Tasks.April;
+
+import java.sql.SQLOutput;
+
+public class Interview_Q_06LargestSubstring {
+
+    public static void main(String[] args) {
+
+        String str = "Shubham is a Automation Engineer";
+
+
+        String[] newString = str.split(" ");
+
+        String largest = newString[0];
+        String smallest = newString[1];
+
+        for (String n : newString) {
+
+            if (largest.toCharArray().length < n.toCharArray().length) {
+                largest = n;
+            } else if (smallest.toCharArray().length > n.toCharArray().length) {
+                smallest = n;
+            }
+
+        }
+        System.out.println("The largest substring is  "+largest);
+        System.out.println("The smallest substring is  "+smallest);
+
+    }
+}
