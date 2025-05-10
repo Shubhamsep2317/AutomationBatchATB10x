7e149b24ce1e6ff4e6bf640fd598f8fc783cf674
Shubham <Shubhamsep23@gmail.com>
2025-05-10 22:37:34 +0530

    10th May interview Question

---
+++ src/Tasks/May/InterviewQ_10_May2025.java
@@ -0,0 +1,30 @@
+package Tasks.May;
+
+public class InterviewQ_10_May2025 {
+    public static void main(String[] args) {
+        int arr[]= {1, 5, 2, 8, 1, 9, 1};
+
+        int eliminateValue=1;
+        int count=0;
+
+        for(int value:arr){
+            if(value==eliminateValue){
+                count++;
+            }
+        }
+
+        int newArr[]= new int[arr.length-count];
+
+        int index=0;
+
+        for (int value:arr){
+            if(value!=eliminateValue){
+                newArr[index++]=value;
+            }
+        }
+
+        for (int value:newArr){
+            System.out.print(value+ "," );
+        }
+    }
+}
