f24c85d4c92c51302567a02d6262ab5bd383621d
Shubham <Shubhamsep23@gmail.com>
2025-05-18 22:29:07 +0530

    Modified add method in feature-branch

--- src/Git_Conflict/Calculator.java
+++ src/Git_Conflict/Calculator.java
@@ -2,6 +2,7 @@ package Git_Conflict;
 
 public class Calculator {
     public int add(int a, int b) {
+        System.out.println("Adding numbers in feature branch");
         return a + b;
     }
 }
