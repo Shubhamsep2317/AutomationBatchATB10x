e04c9c712f79aeadd295add516582c1626690eaf
Shubham <Shubhamsep23@gmail.com>
2025-05-18 23:02:34 +0530

    Test

--- src/Git_Conflict/Demo.java
+++ src/Git_Conflict/Demo.java
@@ -1,6 +1,10 @@
 package Git_Conflict;
 
 public class Demo {
+
+    public int test(int a, int b){
+        return a+b+3-123;
+    }
     public static void main(String[] args) {
         System.out.println("");
     }
