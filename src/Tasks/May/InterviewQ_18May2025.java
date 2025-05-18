3fd1e0d93f29e7d3ab1334955603ed3ce5f02626
Shubham <Shubhamsep23@gmail.com>
2025-05-18 20:27:33 +0530

    18th May interview Question

---
+++ src/Tasks/May/InterviewQ_18May2025.java
@@ -0,0 +1,27 @@
+package Tasks.May;
+// WAP to count the duplicate number of words in a String
+import java.util.*;
+
+public class InterviewQ_18May2025 {
+    public static void main(String[] args) {
+
+        String name = "My name is Shubham and Shubham is a SDET";
+
+        String[] splitName = name.split(" ");
+
+        HashMap<String, Integer> hm = new HashMap();
+
+        for (int i = 0; i < splitName.length; i++) {
+            if (hm.containsKey(splitName[i])) {
+                hm.put(splitName[i], hm.get(splitName[i]) + 1);
+            } else {
+                hm.put(splitName[i], 1);
+            }
+        }
+
+        for (Map.Entry<String, Integer> item : hm.entrySet()) {
+            System.out.println("The character " + item.getKey() + " has occured " + item.getValue() + " times");
+        }
+
+    }
+}
