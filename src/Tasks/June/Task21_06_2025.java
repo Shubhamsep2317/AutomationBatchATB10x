b145536b928294c7cd395abf3017a870b53c231c
Shubham <Shubhamsep23@gmail.com>
2025-06-21 20:53:04 +0530

    New push

---
+++ src/Tasks/June/Task21_06_2025.java
@@ -0,0 +1,21 @@
+package Tasks.June;
+
+public class Task21_06_2025 {
+
+    public static void main(String[] args) {
+        String name="Satish Kumar Sharma";
+
+        String[] splitName=name.split(" ");
+
+
+        StringBuilder sb= new StringBuilder();
+
+        for (int i=0;i<splitName.length-1;i++){
+            sb.append(splitName[i].charAt(0)).append(".");
+        }
+
+        sb.append(splitName[splitName.length-1]);
+
+        System.out.println(sb.toString());
+    }
+}
