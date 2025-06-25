361d60cf2e5016ed837e723fc6d9f2e8118c8734
Shubham <Shubhamsep23@gmail.com>
2025-06-25 17:06:20 +0530

    All commit 25th June 2025

---
+++ src/Tasks/June/Task_25_06_2025.java
@@ -0,0 +1,44 @@
+package Tasks.June;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task_25_06_2025 {
+    public static void main(String[] args) {
+
+
+        int[] arr={10,10,10,30,30,58};
+
+        HashMap<Integer,Integer> hm= new HashMap();
+
+        for (int i:arr){
+            if(hm.containsKey(i)){
+                hm.put(i,hm.get(i)+1);
+            }
+            else{
+                hm.put(i,1);
+            }
+        }
+
+        int maxValue=0;
+        int secondMaxValue=0;
+
+        for (int i:hm.values()){
+            if(i>maxValue){
+                secondMaxValue=maxValue;
+                maxValue=i;
+            }
+            else if(i<maxValue && i>secondMaxValue){
+                secondMaxValue=i;
+            }
+        }
+
+        for (Map.Entry<Integer,Integer> item:hm.entrySet()){
+            if (item.getValue()==secondMaxValue){
+                System.out.println(" "+item.getKey());
+            }
+        }
+
+
+    }
+}
