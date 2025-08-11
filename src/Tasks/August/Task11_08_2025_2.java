30ff5ff62ffe05f7ddb54f5fabaf76d62789e4ac
Shubham <Shubhamsep23@gmail.com>
2025-08-11 19:22:47 +0530

    All commit 11th August 2025

---
+++ src/Tasks/August/Task11_08_2025_2.java
@@ -0,0 +1,23 @@
+package Tasks.August;
+// WAP to extract the matching character at the start and end from a given string say input=abcXYabc
+// output=abc
+public class Task11_08_2025_2 {
+    public static void main(String[] args) {
+
+            String input = "abcXYabc";
+            String result = "";
+
+            for (int i = input.length() / 2; i >= 1; i--) {
+                String prefix = input.substring(0, i);
+                String suffix = input.substring(input.length() - i);
+                if (prefix.equals(suffix)) {
+                    result = prefix;
+                    break;
+                }
+            }
+
+            System.out.println(result);
+
+
+        }
+    }
