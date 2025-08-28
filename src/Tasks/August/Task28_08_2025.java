fc588696b7516be8da77d46f43c9a77ee51c29bc
Shubham <Shubhamsep23@gmail.com>
2025-08-28 12:12:42 +0530

    Changes today 28th August

---
+++ src/Tasks/August/Task28_08_2025.java
@@ -0,0 +1,28 @@
+package Tasks.August;
+
+import java.util.*;
+
+public class Task28_08_2025 {
+    public static void main(String[] args) {
+        String [] arr={"3445","857","0589","0","0698","0119","07"};
+
+        List<String> li=new LinkedList();
+
+        for (int i=0;i<arr.length;i++){
+
+            String input=arr[i];
+            int sum=0;
+            for (char ch:input.toCharArray()){
+                sum+=Character.getNumericValue(ch);
+            }
+
+            if(sum%2!=0){
+                li.add(input);
+            }
+        }
+
+        for (String item:li){
+            System.out.print(" "+item+" ,");
+        }
+    }
+}
