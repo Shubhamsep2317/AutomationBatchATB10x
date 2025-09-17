bc6dfb1e6b99e658095c6163cb9d01eefbcf6f1e
Shubham <Shubhamsep23@gmail.com>
2025-09-17 17:48:47 +0530

    All commit 17th September 2025

---
+++ src/Tasks/September/Task_17_09_2025.java
@@ -0,0 +1,31 @@
+package Tasks.September;
+
+
+// Write a program to remove substrings ab,bc,ade,mn from a given String
+public class Task_17_09_2025 {
+
+    public static void main(String[] args) {
+
+
+        String [] str={"ab","bc","ade","nm"};
+
+        String input="abefbcdeadenmaf";
+
+        boolean found= true;
+
+        while(found){
+            found=false;
+
+            for (String s:str){
+                if(input.contains(s)){
+                    input=input.replaceAll(s,"");
+                    found=true;
+                }
+            }
+        }
+
+        System.out.print(" "+input);
+
+
+    }
+}
