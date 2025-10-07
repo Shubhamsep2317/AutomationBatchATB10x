df4eb72e37bd736218a1e9b38d0da2ff838f1187
Shubham <Shubhamsep23@gmail.com>
2025-10-07 16:35:31 +0530

    Changes 07th October

--- src/Tasks/September/Task_07_10_2025_LargestUniqueSubstring.java
+++ src/Tasks/September/Task_07_10_2025_LargestUniqueSubstring.java
@@ -1,55 +1,50 @@
 package Tasks.September;
+
 // WAP to find the largest unique substring
 public class Task_07_10_2025_LargestUniqueSubstring {
 
-    public static String longestSubstring(String s){
+    public static String longestSubstring(String s) {
 
-        int maxlength=0;
-
-
-
-        int n=s.length();
-        int startAt=0;
-
-        for (int i=0;i<n;i++){
-
-            boolean visited[]= new boolean[256];
+        int maxlength = 0;
 
 
-            for (int j=i;j<n;j++){
-                int currentLength=j-i+1;
+        int n = s.length();
+        int startAt = 0;
 
-                char c=s.charAt(j);
+        for (int i = 0; i < n; i++) {
 
-                if(visited[c]){
+            boolean visited[] = new boolean[256];
+
+
+            for (int j = i; j < n; j++) {
+                int currentLength = j - i + 1;
+
+                char c = s.charAt(j);
+
+                if (visited[c]) {
                     break;
+                } else {
+                    visited[c] = true;
                 }
 
-                else{
-                    visited[c]=true;
-                }
-
-                if(currentLength>maxlength){
-                    maxlength=currentLength;
-                    startAt=i;
+                if (currentLength > maxlength) {
+                    maxlength = currentLength;
+                    startAt = i;
                 }
             }
 
         }
 
-        return s.substring(startAt,startAt+maxlength);
+        return s.substring(startAt, startAt + maxlength);
     }
-
-
-
 
 
     public static void main(String[] args) {
 
 
-        String input= "abcabcd";
+        String input = "abcabcd";
 
-        System.out.print(" "+longestSubstring(input));
+        System.out.print(" " + longestSubstring(input));
 
     }
 }
